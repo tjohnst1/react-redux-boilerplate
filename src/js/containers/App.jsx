a6f090beb9a79a7a2d685b08fc0f9bd26021b5ea
Travis Johnston <tjohnst1@gmail.com>
2017-04-02 21:12:25 -0700

    update redux logger import declaration; update the call store declaration in App.jsx

--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../store/configureStore'
 
+const store = configureStore();
+
 export default class App extends Component {
   render(){
     return (
